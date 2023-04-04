f77baaffa6bdf06d663fd166b43b970f585ee991
jakecernet <117542696+jakecernet@users.noreply.github.com>
2023-04-04 17:17:06 +0200

    modified:   files/script.js 	modified:   files/userarea.css 	modified:   user-area.html

--- files/script.js
+++ files/script.js
@@ -1,2 +1,31 @@
-var menuList = document.getElementById("menuList");
+var whitelist = document.getElementById("whitelist");
+var blacklist = document.getElementById("blacklist");
+var reset = document.getElementById("reset");
+   
+whitelist.style.display = "none";
+blacklist.style.display = "none";
+reset.style.display = "none";
 
+function showWhitelist(){
+    if (whitelist.style.display === "none") {
+        whitelist.style.display = "block";
+    } else {
+        whitelist.style.display = "none";
+    }
+}
+
+function showBlacklist(){
+    if (blacklist.style.display === "none") {
+        blacklist.style.display = "block";
+    } else {
+        blacklist.style.display = "none";
+    }
+}
+
+function resetList(){
+    if (reset.style.display === "none") {
+        reset.style.display = "block";
+    } else {
+        reset.style.display = "none";
+    }
+}
