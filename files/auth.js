98d5190878f7427447f78c6e0efa326c39ed0f59
lin.cadez <lin.cadez@students.vegova.si>
2023-04-06 18:12:08 +0200

    neki novga

--- files/auth.js
+++ files/auth.js
@@ -1,20 +1,7 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
-
-
-createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        // ...
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-    });
+import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyD1fTaLIQOrD96rZUvkxJYYgUt2esHlrYo",
@@ -27,23 +14,82 @@ const firebaseConfig = {
     measurementId: "G-NZB4C0XD2Y"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const auth = getAuth();
 
-signUp.addEventListener("click", (e) => {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
+submit = document.getElementById('submit').value;
 
-    createUserWithEmailAndPassword(auth, email, password)
+document.getElementById("submit").addEventListener("click", function(event){
+    event.preventDefault()
+  });
+
+submit.addEventListener('click',(e) => {
+
+var email = document.getElementById('email').value;
+var password = document.getElementById('password').value;
+var username = document.getElementById('username').value;
+
+createUserWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
-    // Signed in 
+   // Signed in 
     const user = userCredential.user;
+
+    set(ref(database, 'users/' + user.uid),{
+        username: username,
+        email: email
+    })
+
+    alert('user created!');
     // ...
   })
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
-    // ..
+
+    alert(errorMessage);
+  // ..
   });
+
 });
+
+login.addEventListener('click',(e)=>{
+ var email = document.getElementById('email').value;
+ var password = document.getElementById('password').value;
+
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+
+      const dt = new Date();
+       update(ref(database, 'users/' + user.uid),{
+        last_login: dt,
+      })
+
+       alert('User loged in!');
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+
+      alert(errorMessage);
+});
+
+});
+
+const user = auth.currentUser;
+onAuthStateChanged(auth, (user) => {
+if (user) {
+  const uid = user.uid;
+  //bla bla bla
+  // ...
+} else {
+  // User is signed out
+  // ...
+  //bla bla bla
+}
+});
+
