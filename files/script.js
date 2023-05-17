3954230426f571da99e6dbbbc47b4d29e293051a
jakecernet <117542696+jakecernet@users.noreply.github.com>
2023-05-17 18:03:02 +0200

    some changes

--- files/script.js
+++ files/script.js
@@ -1,42 +1,42 @@
-var whitelist = document.getElementById("whitelist");
 var blacklist = document.getElementById("blacklist");
+var whitelist = document.getElementById("meniblk");
 var reset = document.getElementById("reset");
-   
+
 whitelist.style.display = "none";
 blacklist.style.display = "none";
 reset.style.display = "none";
 
-function showWhitelist(){
+function showWhitelist() {
     if (whitelist.style.display === "none") {
         whitelist.style.display = "block";
     } else {
         whitelist.style.display = "none";
     }
-    if (blacklist.style.display === "block" || reset.style.display === "block"){
+    if (blacklist.style.display === "block" || reset.style.display === "block") {
         blacklist.style.display = "none";
         reset.style.display = "none";
     }
 }
 
-function showBlacklist(){
+function showBlacklist() {
     if (blacklist.style.display === "none") {
         blacklist.style.display = "block";
     } else {
         blacklist.style.display = "none";
     }
-    if (reset.style.display === "block" || whitelist.style.display === "block"){
+    if (reset.style.display === "block" || whitelist.style.display === "block") {
         reset.style.display = "none";
         whitelist.style.display = "none";
     }
 }
 
-function resetList(){
+function resetList() {
     if (reset.style.display === "none") {
         reset.style.display = "block";
     } else {
         reset.style.display = "none";
     }
-    if (blacklist.style.display === "block" || whitelist.style.display === "block"){
+    if (blacklist.style.display === "block" || whitelist.style.display === "block") {
         blacklist.style.display = "none";
         whitelist.style.display = "none";
     }
