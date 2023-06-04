bcb2289a564a3b04a4583a3d77de4e626dcd361d
lin.cadez <lin.cadez@students.vegova.si>
2023-06-04 13:33:05 +0200

    dodajam kodo za login in display menija

--- analiza.js
+++ analiza.js
@@ -1,1 +1,18 @@
-console.log('Jaka, ne tega premaknt drgam');
+const kroky = require('kroky-api');
+
+
+async function fetchMeals() {
+    await kroky.login("ČadLin", "hlamehhit");
+    console.log(await kroky.getMeals(1));
+  }
+  
+fetchMeals();
+
+async function selectMeal(date, id, xl) {
+  await kroky.login("ČadLin", "hlamehhit");
+  await kroky.selectMeal(date, id, xl);
+  console.log("narročeno");
+}
+
+selectMeal("2023-06-16", 30752, true);
+
