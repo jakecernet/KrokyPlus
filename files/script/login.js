355e005c7b380f1c0564db098d704b72a2dba2ba
lin.cadez <lin.cadez@students.vegova.si>
2023-06-05 16:19:02 +0200

    dodajam funkcijo za login

--- files/script/login.js
+++ files/script/login.js
@@ -1,6 +1,24 @@
+async function fetchMeals(username, password) {
+    await kroky.login(username, password);
+    return await kroky.getMeals(1);
+  }
+
 function login() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     document.cookie = "username=" + username;
     document.cookie = "password=" + password;
+    window.open("/user-area.html", "_blank");
 }
+
+
+function display(){
+    let username = getCookie("username");
+    let password = getCookie("password");
+    const userDisplayWidget = document.getElementById("user");
+    userDisplayWidget.innerHTML = username + " " + password;
+    const MenuDisplayWidget = document.getElementById("test123"); 
+    fetchMeals(username, password)
+
+
+}   
