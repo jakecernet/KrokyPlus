5abec84a72537b474b99ea4d4613c8c372ec422b
Jaka Černetič <117542696+jakecernet@users.noreply.github.com>
2023-06-22 19:08:38 +0200

    animacije v user area

--- files/script/script.js
+++ files/script/script.js
@@ -1,96 +1,101 @@
 var blacklist = document.getElementById("blacklist");
 var whitelist = document.getElementById("meniblk");
 var reset = document.getElementById("reset");
+var meniji = document.getElementById("meniji");
 
+whitelist.style.height = "0px";
 whitelist.style.display = "none";
 blacklist.style.display = "none";
+blacklist.style.height = "0px";
 reset.style.display = "none";
 
 function showWhitelist() {
-    if (whitelist.style.display === "none") {
-        whitelist.style.display = "block";
-    } else {
-        whitelist.style.display = "none";
-    }
-    if (blacklist.style.display === "block" || reset.style.display === "block") {
-        blacklist.style.display = "none";
-        reset.style.display = "none";
-    }
+  if (whitelist.style.height === "0px") {
+    whitelist.style.display = "block";
+    setTimeout(function () {
+      whitelist.style.height = "600px";
+      blacklist.style.height = "0px";
+      reset.style.height = "0px";
+    }, 10);
+  }
+  else {
+    whitelist.style.height = "0px";
+    setTimeout(function () { whitelist.style.display = "none"; }, 300);
+  }
 }
 
 function showBlacklist() {
-    if (blacklist.style.display === "none") {
-        blacklist.style.display = "block";
-    } else {
-        blacklist.style.display = "none";
-    }
-    if (reset.style.display === "block" || whitelist.style.display === "block") {
-        reset.style.display = "none";
-        whitelist.style.display = "none";
-    }
+  if (blacklist.style.height === "0px") {
+    blacklist.style.display = "block";
+    setTimeout(function () {
+      blacklist.style.height = "9050px";
+      whitelist.style.height = "0px";
+      reset.style.height = "0px";
+    }, 10);
+  }
+  else {
+    blacklist.style.height = "0px";
+    setTimeout(function () { blacklist.style.display = "none"; }, 300);
+  }
 }
 
 function resetList() {
-    if (reset.style.display === "none") {
-        reset.style.display = "block";
-    } else {
-        reset.style.display = "none";
-    }
-    if (blacklist.style.display === "block" || whitelist.style.display === "block") {
-        blacklist.style.display = "none";
-        whitelist.style.display = "none";
-    }
+  if (reset.style.height === "0px") {
+    reset.style.display = "block";
+    setTimeout(function () {
+      reset.style.height = "180px";
+      blacklist.style.height = "0px";
+      whitelist.style.height = "0px";
+    }, 10);
+  }
+  else {
+    reset.style.height = "0px";
+    setTimeout(function () { reset.style.display = "none"; }, 300);
+  }
 }
 
 function checkbox(num) {
-    var checkbox = document.getElementById(num);
-    if (checkbox.checked == true) {
-        checkbox.checked = false;
-    } else {
-        checkbox.checked = true;
-    }
+  var checkbox = document.getElementById(num);
+  if (checkbox.checked == true) {
+    checkbox.checked = false;
+  } else {
+    checkbox.checked = true;
+  }
 }
 
-function setCookie(name, value, days) {
-      var expires = "";
-      if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-      }
-      document.cookie = name + "=" + (value || "") + expires + "; path=/";
+function save() {
+  var whitelist = document.getElementById("meniblk");
+  var blacklist = document.getElementById("blacklist");
+  var reset = document.getElementById("reset");
+  var whitelistArray = [];
+  var blacklistArray = [];
+  var resetArray = [];
+  var whitelistChecked = whitelist.getElementsByTagName("input");
+  var blacklistChecked = blacklist.getElementsByTagName("input");
+  var resetChecked = reset.getElementsByTagName("input");
+  for (var i = 0; i < whitelistChecked.length; i++) {
+    if (whitelistChecked[i].checked) {
+      whitelistArray.push(whitelistChecked[i].value);
     }
-
-    function getCookie(name) {
-      var nameEQ = name + "=";
-      var ca = document.cookie.split(';');
-      for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-      }
-      return null;
+  }
+  for (var i = 0; i < blacklistChecked.length; i++) {
+    if (blacklistChecked[i].checked) {
+      blacklistArray.push(blacklistChecked[i].value);
     }
-
-    function acceptCookies() {
-      setCookie("cookieAccepted", "true", 365);
-      hideCookiePopup();
+  }
+  for (var i = 0; i < resetChecked.length; i++) {
+    if (resetChecked[i].checked) {
+      resetArray.push(resetChecked[i].value);
     }
-
-    function rejectCookies() {
-      setCookie("cookieAccepted", "false", 365);
-      hideCookiePopup();
-    }
-
-    function hideCookiePopup() {
-      document.getElementById("cookie-popup").style.display = "none";
-    }
-
-    function showCookiePopup() {
-      document.getElementById("cookie-popup").style.display = "block";
-    }
-
-    var cookieAccepted = getCookie("cookieAccepted");
-    if (cookieAccepted === null) {
-      showCookiePopup();
-    }
+  }
+  chrome.storage.sync.set({ "whitelist": whitelistArray }, function () {
+    console.log("whitelist saved");
+  });
+  chrome.storage.sync.set({ "blacklist": blacklistArray }, function () {
+    console.log("blacklist saved");
+  });
+  chrome.storage.sync.set({ "reset": resetArray }, function () {
+    console.log("reset saved");
+  });
+  window.close();
+}
