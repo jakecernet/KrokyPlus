db924645ca7c82031566b4ceb8d1f48799690cde
lin-cadez <lin.cadez@gmail.com>
2023-08-15 00:20:10 +0200

    slika sole in stast

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-function displayMenu() {
-    menu = document.getElementById("menu");
-    if (menu.style.height == "0px") {
-        menu.style.height = "300px";
-    } else {
-        menu.style.height = "0px";
-    }
-}
+
+document.getElementById("image-widget").style.display = "block";
+document.getElementById("stats-widget").style.display = "none";
 
 fetch("https://kroky-plus-backend.vercel.app/api/stat")
     .then((res) => res.json())
@@ -13,9 +8,13 @@ fetch("https://kroky-plus-backend.vercel.app/api/stat")
         const number_of_users = data.number_users;
         document.getElementById("number_of_users").innerHTML =
             number_of_users;
+        const number_of_views = data.number_views;
+        document.getElementById("number_of_views").innerHTML =
+            number_of_views;
+            
     })
     .catch((err) => {
-        const number_of_users = "mnogo";
-        document.getElementById("number_of_users").innerHTML =
-            number_of_users;
-    });
+        document.getElementById("image-widget").style.display = "block";
+        document.getElementById("stats-widget").style.display = "none";
+
+    }); 
