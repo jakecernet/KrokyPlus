00aa80530fc3fea5a00adea498382db9d379ab7d
Jaka Černetič <jaka.cernetic@students.vegova.si>
2023-08-15 14:08:35 +0200

    mal popravkov

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-document.getElementById("image-widget").style.display = "block";
-document.getElementById("stats-widget").style.display = "none";
+document.getElementById("image-widget").style.display = "none";
+document.getElementById("stats-widget").style.display = "flex";
 
 function displayMenu() {
     menu = document.getElementById("menu");
