6204007f64ce9b9d8f4c45c0bd886269c0b3fdbe
Jaka Černetič <jaka.cernetic@students.vegova.si>
2023-09-14 21:09:27 +0200

    par lighthouse fixou

--- files/script.js
+++ files/script.js
@@ -13,8 +13,4 @@ fetch("https://kroky-plus-backend.vercel.app/api/stat")
         const number_of_users = data.number_users;
         document.getElementById("number_of_users").innerHTML =
             number_of_users;
-        const number_of_views = data.number_views;
-        document.getElementById("number_of_views").innerHTML =
-            number_of_views;
-
     });
