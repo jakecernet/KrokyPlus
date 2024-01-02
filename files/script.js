7b5ff137348c34b1e783a961a26deb8614f569ca
Jaka Černetič <jaka.cernetic@students.vegova.si>
2024-01-02 11:37:00 +0100

    lighthouse optimisation

--- files/script.js
+++ files/script.js
@@ -1,16 +1,15 @@
 function displayMenu() {
-    menu = document.getElementById("menu");
-    if (menu.style.left == "0%") {
-        menu.style.left = "-100%";
-    } else {
-        menu.style.left = "0%";
-    }
+	menu = document.getElementById("menu");
+	if (menu.style.left == "0%") {
+		menu.style.left = "-100%";
+	} else {
+		menu.style.left = "0%";
+	}
 }
 
 fetch("https://kroky-plus-backend.vercel.app/api/stat")
-    .then((res) => res.json())
-    .then((data) => {
-        const number_of_users = data.number_users;
-        document.getElementById("number_of_users").innerHTML =
-            number_of_users;
-    });
+	.then((res) => res.json())
+	.then((data) => {
+		const number_of_users = data.number_users;
+		document.getElementById("number_of_users").innerHTML = number_of_users;
+	});
